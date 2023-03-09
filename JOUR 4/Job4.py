bcbefd395ae11610d2ec1242e827e7c8b7ec9dd1
ema-elkouh <ema.el-kouh@laplateforme.io>
2023-03-09 10:28:05 +0100

    Update Job4.py

--- JOUR 4/Job4.py
+++ JOUR 4/Job4.py
@@ -1,5 +1,3 @@
-fruits = ["pomme", "cerise", "orange", "melon"]
-print(fruits[0])
-print(fruits[1])
-print(fruits[2])
-print(fruits[3])
+fruits = ['pomme', 'cerise', 'orange', 'melon']
+fruits.insert(2, 'mangue')
+print(fruits)
