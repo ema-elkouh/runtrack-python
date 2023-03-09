949f725119048661d995c58e441d6006c74c9f8c
ema-elkouh <ema.el-kouh@laplateforme.io>
2023-03-09 10:24:05 +0100

    Update Job3.py

--- JOUR 4/Job3.py
+++ JOUR 4/Job3.py
@@ -1,5 +1,3 @@
-fruits = ["pomme", "cerise", "orange", "melon"]
-print(fruits[0])
-print(fruits[1])
-print(fruits[2])
-print(fruits[3])
+fruits = ['pomme', 'cerise', 'orange']
+fruits.append('Melon')
+print(fruits)
