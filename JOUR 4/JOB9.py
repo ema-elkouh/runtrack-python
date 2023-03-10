edbe4b2e8625da04f83c7cfdb349ecd931da393c
ema-elkouh <ema.el-kouh@laplateforme.io>
2023-03-10 10:32:16 +0100

    Update JOB9.py

--- JOUR 4/JOB9.py
+++ JOUR 4/JOB9.py
@@ -1,8 +1,10 @@
-L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
-L.sort()
-nummini = L[0]
-print(nummini)
+L= [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
+my_min = L[0]
+my_max = L[0]
 
-L.sort(reverse=True)
-nummaxi = L[0]
-print(nummaxi)
+for i in L:
+    if i <= my_min:
+        my_min=i
+    if i >= my_max:
+        my_max=i
+print(my_max+my_min)
